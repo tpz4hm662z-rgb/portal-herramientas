0c87d61f4ecd2da36b861081a60ac0172eabded1
tpz4hm662z-rgb <tpz4hm662z@privaterelay.appleid.com>
2026-07-02 13:18:49 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//futuras funcionalidades
+function buscarHerramientas() {
+    const texto = document.getElementById("buscador").value.toLowerCase();
+    const tarjetas = document.querySelectorAll(".tarjeta");
+
+    tarjetas.forEach(function(tarjeta) {
+        const contenido = tarjeta.textContent.toLowerCase();
+
+        if (contenido.includes(texto)) {
+            tarjeta.style.display = "";
+        } else {
+            tarjeta.style.display = "none";
+        }
+    });
+}
